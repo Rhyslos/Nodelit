a7b53256c8cf9bc9003b5ef06e2263bf04d2ec58
Yuna Emiya <yuna-emiya@outlook.com>
2026-08-18 14:47:10 +0200

    fixed calendar export

--- Server/api/CalendarAPI.mjs
+++ Server/api/CalendarAPI.mjs
@@ -1,6 +1,6 @@
 // import modules
 import { Router } from 'express';
-import { db } from '../database/Database.mjs';
+import db from '../database/Database.mjs';
 import { broadcastToWorkspace } from '../modules/Networking.mjs';
 import {
     requireText,
